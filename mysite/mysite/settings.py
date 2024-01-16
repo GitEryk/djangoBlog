966818a93352e575d6c353cc70b8847751969884
Eryk Urban <urb2001an@gmail.com>
2024-01-16 10:52:18 +0100

    Usunięcie dostępu do serwra smtp

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -116,9 +116,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "urb2001an@gmail.com"
-EMAIL_HOST_PASSWORD = "fxwf yjit xzkn hevm"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
